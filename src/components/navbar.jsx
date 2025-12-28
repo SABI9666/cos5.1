d4e2db2563a386adc4aafdd3b425bd92aff42ea4
SABI9666 <sabincn676@gmail.com>
2025-12-28 10:31:10 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -137,7 +137,7 @@ function Navbar(props) {
     <React.Fragment>
       <nav className={scrolled ? "navbar scrolled" : "navbar"}>
         <div className="navbar-container">
-          <Link to="/" className="logo">LUXELED</Link>
+          <Link to="/" className="logo">LaxoraLED</Link>
           
           <ul className="nav-links desktop-nav">
             <li>
